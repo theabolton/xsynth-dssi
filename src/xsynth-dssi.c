bc9fa07aff2ec5c3055561210b2177f08106335a
Chris Cannam <cannam@users.sourceforge.net>
2004-07-13 15:12:56 +0000

    * select Third Way for get_program

--- src/xsynth-dssi.c
+++ src/xsynth-dssi.c
@@ -248,6 +248,7 @@ xsynth_configure(LADSPA_Handle instance, const char *key, const char *value)
  * implements DSSI (*get_program)()
  */
 // !FIX! get_program():
+#define DO_GET_PROGRAM_THE_THIRD_WAY 1
 #ifdef DO_GET_PROGRAM_THE_THIRD_WAY
 const DSSI_Program_Descriptor *
 xsynth_get_program(LADSPA_Handle instance, unsigned long index)
